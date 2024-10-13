af03c2081d3fc7070c3824d50f6ad2e3117ddd89
awolschi <157239149+awolschi@users.noreply.github.com>
2024-10-13 18:04:01 +0200

    Added ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,8 +6,23 @@
 /*   By: awolschi <awolschi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:40:04 by awolschi          #+#    #+#             */
-/*   Updated: 2024/10/13 17:02:25 by awolschi         ###   ########.fr       */
+/*   Updated: 2024/10/13 18:02:09 by awolschi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*b_copy;
+
+	i = 0;
+	b_copy = (unsigned char *)b;
+	while (i < len)
+	{
+		b_copy[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
