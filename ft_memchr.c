4964b9f3289e331b316de1dec70aa0ca6c643655
awolschi <157239149+awolschi@users.noreply.github.com>
2024-10-13 19:31:23 +0200

    Added ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awolschi <awolschi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 18:27:16 by awolschi          #+#    #+#             */
+/*   Updated: 2024/10/13 19:27:14 by awolschi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*str;
+	unsigned char		uc;
+	size_t				i;
+
+	str = (unsigned char *)s;
+	uc = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == uc)
+		{
+			return ((void *)&str[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
