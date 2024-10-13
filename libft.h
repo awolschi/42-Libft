67408d6f6e4aafec7bc64ac770dbf86b7a0210cf
awolschi <157239149+awolschi@users.noreply.github.com>
2024-10-13 18:04:51 +0200

    Modified Makefile and header to include memset

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: awolschi <awolschi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 17:14:38 by awolschi          #+#    #+#             */
-/*   Updated: 2024/10/13 17:43:31 by awolschi         ###   ########.fr       */
+/*   Updated: 2024/10/13 18:02:43 by awolschi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int					ft_isalpha(int c);
 int					ft_isascii(int c);
 int					ft_isdigit(int c);
 int					ft_isprint(int c);
+void				*ft_memset(void *b, int c, size_t len);
 char				*ft_strchr(char *s, int c);
 int					ft_strlen(const char *str);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
