c38120dd4cde908d4591c9e0cc3c206e2a61c7be
awolschi <157239149+awolschi@users.noreply.github.com>
2024-11-01 20:28:03 +0100

    Worked on normerrors. Now everythings good :)

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,17 +6,18 @@
 /*   By: awolschi <awolschi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 18:30:13 by awolschi          #+#    #+#             */
-/*   Updated: 2024/11/01 20:17:23 by awolschi         ###   ########.fr       */
+/*   Updated: 2024/11/01 20:25:06 by awolschi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/// @brief Deletes and frees the given element and every successor of that element,
-/// using the function 'del' and free(3). Finally,
+/// @brief Deletes and frees the given element and every successor of that
+/// element,using the function 'del' and free(3). Finally,
 ///	the pointer to the list must be set to NULL.
 /// @param lst The address of a pointer to an element.
-/// @param del The address of a function used to delete the content of the element.
+/// @param del The address of a function used to delete the content of the
+/// element.
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
